90b0edbdc6dac4e3bd07583878f63001158e349d
Mariel F <marielbeth@gmail.com>
2017-09-09 16:39:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -36,13 +36,13 @@ class App < Sinatra::Base
     @number2 = params[:number2].to_i
     case @operation
     when "add"
-      @number1 + @number2
+      "#{@number1 + @number2}"
     when "subtract"
-      @number1 - @number2
+      "#{@number1 - @number2}"
     when "multiply"
-      @number1 * @number2
+      "#{@number1 * @number2}"
     when "divide"
-      @number1 / @number2
+      "#{@number1 / @number2}"
     end
   end
 
